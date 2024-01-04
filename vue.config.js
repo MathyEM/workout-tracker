d2d625cc3b9ae24ad8e51a56e8593b2b20d09946
MathyEM <72448225+MathyEM@users.noreply.github.com>
2024-01-04 16:28:44 +0100

    Added _variables.scss

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+        sass: {
+            sourceMap: false,
+            additionalData: `
+            @import "@/assets/scss/_variables.scss";
+            `,
+        }
+    }
+  },
 })
