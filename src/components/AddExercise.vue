466535d2c6be73f717ee914f13a2f88034d5bd18
MathyEM <72448225+MathyEM@users.noreply.github.com>
2024-01-07 17:10:32 +0100

    Adjusted styling of AddExercise

--- src/components/AddExercise.vue
+++ src/components/AddExercise.vue
@@ -17,9 +17,22 @@ export default {
   width: auto;
 }
 .plus-btn {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   background-color: transparent;
   border: none;
-  font-size: 5rem;
+  font-size: 3.75rem;
   color: $blue;
+  text-shadow: 1px 1px 1px rgb(0 0 0 / 20%);
+  transition: text-shadow 150ms ease-out;
+}
+.plus-btn:active {
+  text-shadow: 0px 0px 0px rgb(0 0 0 / 20%);
+}
+.plus-btn::after {
+  content: "Create New Program";
+  font-size: 0.5em;
+  margin-top: -0.6em;
 }
 </style>
