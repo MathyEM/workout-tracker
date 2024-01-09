344d0f160b4614409c7b2963f7e731b736ffe140
MathyEM <72448225+MathyEM@users.noreply.github.com>
2024-01-09 13:08:57 +0100

    Created Vuex module createProgram

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import createProgram from './modules/createProgram'
 
 export default createStore({
   state: {
@@ -10,5 +11,6 @@ export default createStore({
   actions: {
   },
   modules: {
+    createProgram,
   }
 })
