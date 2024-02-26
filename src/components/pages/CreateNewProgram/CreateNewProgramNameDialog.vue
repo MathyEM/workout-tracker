7de30207860d17574bcc46536ca93269906f4d18
MathyEM <72448225+MathyEM@users.noreply.github.com>
2024-02-26 11:31:51 +0100

    Updated and moved CreateNewProgram to a dialog and seperate component

---
+++ src/components/pages/CreateNewProgram/CreateNewProgramNameDialog.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="create-new-program">
+    <v-btn class="plus-btn" color="primary" @click="dialog = true">
+      <span class="plus">+</span>
+      <h2>Create New Program</h2>
+    </v-btn>
+  </div>
+  <v-dialog class="create-new-program" v-model="dialog" width="auto">
+    <v-card max-width="400" title="Choose a name for you new program">
+      <v-card-text>
+        <v-text-field clearable label="Program name" variant="solo"></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <router-link class="btn-link" to="/add-exercise">
+          <v-btn text="Close Dialog" @click="dialog = false"></v-btn>
+        </router-link>
+    </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+  export default {
+    name: 'CreateNewProgramNameDialog',
+    data() {
+      return {
+        dialog: false,
+      }
+    }
+  }
+</script>
+
+<style scoped lang='scss'>
+.create-new-program {
+  position: fixed;
+  bottom: 0;
+}
+.plus-btn, .v-btn__content {
+  height: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: transparent;
+  border: none;
+  color: white;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  user-select: none;
+
+  span.plus {
+    font-size: 3.75rem;
+    line-height: 0.5em;
+  }
+
+  * {
+    margin: 0;
+  }
+}
+.plus-btn:active {
+  text-shadow: 0px 0px 0px rgba(0, 0, 0, 0.5);
+  box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.33);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,23 +2,14 @@
   <div class="home">
     <!-- <p>{{ categories[0] }}</p> -->
     <ProgramList />
-    <div class="create-new-program">
-      <router-link class="btn-link" to="/add-exercise">
-        <VBtn class="plus-btn" color="primary">
-        <!-- <button type="button" class="plus-btn"> -->
-          <span class="plus">+</span>
-          <h2>Create New Program</h2>
-        <!-- </button> -->
-        </VBtn>
-      </router-link>
-    </div>
+    <CreateNewProgramNameDialog />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import ProgramList from '@/components/ProgramList.vue'
-import {VBtn} from 'vuetify/lib/components/VBtn'
+import CreateNewProgramNameDialog from '@/components/pages/CreateNewProgram/CreateNewProgramNameDialog.vue'
 
 import { mapGetters } from 'vuex';
 
@@ -26,7 +17,7 @@ export default {
   name: 'HomeView',
   components: {
     ProgramList,
-    VBtn
+    CreateNewProgramNameDialog,
   },
   data() {
     return {
@@ -41,49 +32,12 @@ export default {
 }
 </script>
 <style lang="scss">
-  .home {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    height: 100%;
-    width: 100%;
-    overflow: auto;
-  }
-
-  .create-new-program {
-  background-color: $blue;
-  position: fixed;
-  bottom: 0;
-
-  .btn-link {
-    text-decoration: none;
-  }
-  
-  .plus-btn, .v-btn__content {
-    height: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: transparent;
-    border: none;
-    color: white;
-    padding-top: 10px;
-    padding-bottom: 10px;
-    user-select: none;
-
-    span.plus {
-      font-size: 3.75rem;
-      line-height: 0.5em;
-    }
-
-    * {
-      margin: 0;
-    }
-  }
-  .plus-btn:active {
-    text-shadow: 0px 0px 0px rgba(0, 0, 0, 0.5);
-    box-shadow: 0px 0px 0px 0px rgba(0, 0, 0, 0.33);
-  }
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+  width: 100%;
+  overflow: auto;
 }
-
 </style>
