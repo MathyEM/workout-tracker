b8ce9e01458bdfa547805ecdee4fddf57344ddd2
MathyEM <72448225+MathyEM@users.noreply.github.com>
2024-02-26 11:33:30 +0100

    Prepared component for creating a new program

---
+++ src/views/CreateNewExercise.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='create-new-exercise'>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CreateNewExercise',
+  }
+</script>
+
+<style scoped lang='scss'>
+  .create-new-exercise {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+  }
+</style>
