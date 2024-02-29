e5a81e3497d06c585a2b89037a0b17f481e6e675
Mathias Eggert Marloth <dj-ert@hotmail.com>
2024-02-29 15:10:01 +0100

    Added base layout and an example program data structure

--- src/store/modules/createProgram.js
+++ src/store/modules/createProgram.js
@@ -1,5 +1,51 @@
 const state = {
-  
+  programName: "",
+  programDescription: "",
+  programDays: [
+    /*
+    {
+      name: "Back day", // unique (within this program)
+      notes: "The day you train back. Focus on xyz etc..."
+      exercises: [
+        {
+          hash: 3357665225, //lat pulldown
+          repRange: "6-15" //regex allow one or more numbers [0-9] potentially followed by "-" followed by one or more numbers [0-9]
+          notes: "Focus on a slow and controlled negative",
+        },
+        {
+          hash: xyz, //Seated Cable Rows
+          repRange: "6-15"
+          notes: "Focus on a slow and controlled negative",
+        }
+      ]
+    },
+    {
+      name: "Chest day",
+      notes: "The day you train chest. Focus on xyz etc..."
+      exercises: [
+        {
+          hash: xyz, //Bench Press
+          repRange: "6-15"
+          notes: "Focus on a slow and controlled negative",
+        },
+        {
+          hash: xyz, //Cable Flys
+          repRange: "6-15"
+          notes: "Focus on a slow and controlled negative",
+        }
+      ]
+    }
+    */
+  ],
+  recommendedSchedule: {
+    mon: 0,
+    tue: 1,
+    wed: 2,
+    thu: 3,
+    fri: 4,
+    sat: 5,
+    sun: 6,
+  }
 }
 
 const getters = {
