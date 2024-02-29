d51ca4522f5b7ecc2b8152f782b5cdbf38b1b9b8
Mathias Eggert Marloth <dj-ert@hotmail.com>
2024-02-29 11:33:45 +0100

    Rename view

---
+++ src/views/CreateNewProgram.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='create-new-program'>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CreateNewProgram',
+  }
+</script>
+
+<style scoped lang='scss'>
+  .create-new-program {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+  }
+</style>
---
+++ src/views/CreateProgram.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class='create-program'>
+    
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'CreateProgram',
+  }
+</script>
+
+<style scoped lang='scss'>
+  .create-program {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5em;
+  }
+</style>
