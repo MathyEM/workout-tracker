ddc3a279d3af9ff4e79018cedc884121bcafcf02
MathyEM <72448225+MathyEM@users.noreply.github.com>
2024-03-07 10:35:12 +0100

    Added training day card component for CreateProgram

---
+++ src/components/pages/CreateNewProgram/ProgramTrainingDayCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card>
+    <v-card-title>{{ title || 'Training day' }}</v-card-title>
+    <v-card-subtitle>
+      <p v-if="exercises.length < 1">No exercises added yet</p>
+      <ul v-else>
+        <li v-for="(exercise, index) in exercises.slice(0, 5)" :key="index">{{ exercise.name }}</li>
+      </ul>
+
+    </v-card-subtitle>
+
+    <v-card-actions class="card-actions">
+      <v-btn icon="mdi-pencil" rounded="lg" density="comfortable"/>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+    name: 'ProgramTrainingDayCard',
+    props: {
+      title: String,
+      exercises: Array,
+    },
+  }
+</script>
+
+<style scoped lang='scss'>
+  ul {
+    padding: inherit;
+  }
+</style>
--- src/views/CreateProgram.vue
+++ src/views/CreateProgram.vue
@@ -4,6 +4,7 @@
       <ProgramName/>
       <ProgramDescription/>
       <AddNewProgramDayButton/>
+      <ProgramTrainingDayCard :exercises="[ {name: 'Lat Pulldown' } ]" title=""/>
   </div>
 </template>
 
@@ -11,6 +12,7 @@
 import ProgramName from "@/components/pages/CreateNewProgram/ProgramName.vue"
 import ProgramDescription from "@/components/pages/CreateNewProgram/ProgramDescription.vue"
 import AddNewProgramDayButton from "@/components/pages/CreateNewProgram/AddNewProgramDayButton.vue"
+import ProgramTrainingDayCard from "@/components/pages/CreateNewProgram/ProgramTrainingDayCard.vue"
 
   export default {
     name: 'CreateProgram',
@@ -18,6 +20,7 @@ import AddNewProgramDayButton from "@/components/pages/CreateNewProgram/AddNewPr
       ProgramName,
       ProgramDescription,
       AddNewProgramDayButton,
+      ProgramTrainingDayCard,
     }
   }
 </script>
